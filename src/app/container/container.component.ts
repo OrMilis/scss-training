0078551e07c3db09eca41ae3d2539f2986a6db16
Or Milis <ormilism@gmail.com>
2022-10-01 02:00:04 +0300

    init

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, map, Observable } from 'rxjs';
+
+export const colorNames: string[] = ['primary', 'secondary', 'tertiary'];
+export type Colors = typeof colorNames[number];
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: [
+    './container.component.scss',
+    './ex2.scss',
+    './ex3.scss',
+    './ex4.scss',
+  ],
+})
+export class ContainerComponent implements OnInit {
+  typingText = `Hello, I'm typing text!`;
+  typingObs$!: Observable<number>;
+
+  colors: Record<Colors, string[]> = {
+    primary: [],
+    secondary: [],
+    tertiary: [],
+  };
+
+  colorNames = colorNames;
+
+  constructor() {
+    this.typingObs$ = interval(150).pipe(
+      map((_, index) => index % this.typingText.length)
+    );
+
+    Object.keys(this.colors).forEach((color) => {
+      this.colors[color] = [...new Array(9)].map(
+        (_, index) => `var(--${color}-${(index + 1) * 100})`
+      );
+    });
+  }
+
+  ngOnInit(): void {}
+}
